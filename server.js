f03fd4491f1f406a1c76b7c33f685c7081f25669
mrhossie <hossam.mohyeldin@gmail.com>
2017-05-24 23:34:25 -0400

    Made server side work as well as finalized json file.

---
+++ server.js
@@ -0,0 +1,56 @@
+console.log('server is starting');
+
+var express = require('express');
+var app = express();
+var server = app.listen(3000, listening);
+
+function listening(){
+  console.log("Listening....");
+}
+
+app.use(express.static('public'));
+
+
+var fs = require("fs");
+var data = fs.readFileSync("final.json");
+
+var weights = JSON.parse(data);
+
+
+app.get("/all", sendAll);
+function sendAll(request, response){
+  response.send(weights);
+  console.log("sent data");
+}
+
+app.get("/add/:date/:weight", addWeight);
+function addWeight (request, response){
+
+  var data = request.params;
+  var date = data.date;
+  //enter error handling for dates.
+  var weight = data.weight;
+  //ender error handling for weights
+  weights[date].weight = Number(weight);
+  var reply = {
+    msg: "New weight added"
+  }
+  response.send(reply);
+}
+app.get("/search/:date", searchDate);
+function searchDate(request, response){
+  var date = request.params.date;
+  var reply;
+  if(weights[date]){
+    reply = {
+      status: "found",
+      date: date,
+      data: weights[date]
+
+    }
+  } else{
+    reply = {status: "date not found"}
+  }
+  response.send(reply);
+
+}
