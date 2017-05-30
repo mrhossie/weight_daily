a05a29adf304ea3b88a5a584cd854cb169f3ec52
mrhossie <hossam.mohyeldin@gmail.com>
2017-05-29 23:50:56 -0400

    added POST usage... not working yet!

--- public/sketch.js
+++ public/sketch.js
@@ -2,68 +2,102 @@ var allData;
 var userDate;
 var userWeight;
 var userAttended;
+var uYear;
+var uDay;
+var uMonth;
+var submit;
+var view;
+var analysis;
+var analysisParagraph;
+var uWeight;
+var uAttendance;
 
-function preload(){
-}
+function preload() {}
 
-function setup(){
+function setup() {
   //createCanvas(400,400);
   //background(51);
+  uDate = select("#udate");
+  submit = select("#submit");
+  view = select("#view");
+  analysis = select("#analysis");
+  analysisParagraph = select("#analysisParagraph");
+  uWeight = select("#weight");
+  uAttendance = select("#attendance");
   reloadData();
   console.log("Running...")
-}
-function reloadData(){
-  loadJSON("/all", gotData);
-}
-
-function gotData(data){
-  console.log("Got all Data...");
-  allData=data;
-  console.log(allData);
-  var uYear = select("#uyear");
-  var uDay = select("#uday");
-  var uMonth = select("#umonth");
-  var submit = select("#submit");
-  var view = select("#view");
-  var uWeight = select("#weight");
-  var uAttendance = select("#attendance");
-
   submit.mousePressed(setValues);
   view.mousePressed(showValues);
+  analysis.mousePressed(analyzeData);
+}
 
-  function setValues(){
-    userDate = uYear.value() +"-"+uMonth.value()+"-"+uDay.value();
+
+
+function reloadData() {
+  loadJSON("/all", gotData);
+  console.log("Data reloaded...")
+
+}
+
+function gotData(data) {
+  console.log("Got all Data...");
+  allData = data;
+  showValues();
+}
+
+function setValues() {
+  userDate = uDate.value();
+
+  if (!uWeight.value()){
+    userWeight = allData[userDate].weight;
+  }else{
     userWeight = uWeight.value();
-    if (uAttendance.value()=="yes"){
-        userAttended = true;
-    }else{
-      userAttended = false;
-    }
-    loadJSON("/add/"+userDate+"/"+userWeight, finished);
-    function finished(data){console.log(data);reloadData();}
-
   }
 
+  if(!uAttendance.value()){
+    userAttended = allData[userDate].bjj;
+  }else if (uAttendance.value() == "yes") {
+    userAttended = true;
+  } else if (uAttendance.value() == "no"){
+    userAttended = false;
+  }
+  var r = {data:"this is data"};
+
+  httpPost("/addP", r, "json", finished);
+  //loadJSON("/add/" + userDate + "/" + userWeight + "/" + userAttended, finished);
+
+}
+
+function finished(d) {
+  console.log(d);
+  reloadData();
+}
+function clearFields(){
+  //this happens in setValues to clear all fields.
+}
 
 
-  function showValues(){
-    var viewP = select("#viewData");
-    viewP.html("");
-    var keys = Object.keys(allData);
-    for (var i =0; i<keys.length;i++){
+function showValues() {
+  var viewP = select("#viewData");
+  viewP.html("");
+  var keys = Object.keys(allData);
+  for (var i = 0; i < keys.length; i++) {
 
-      var line = "Date: " + keys[i] + " Weight: " + allData[keys[i]].weight;
-      if (allData[keys[i]].bjj){
-        line += " attended: yes "
-      }else{
-        line += " attended: no "
-      }
-      line+="</br>"
-      viewP.html(line, true);
+    var line = "Date: " + keys[i] + " Weight: " + allData[keys[i]].weight;
+    if (allData[keys[i]].bjj) {
+      line += " attended: yes "
+    } else {
+      line += " attended: no "
     }
-
+    line += "</br>"
+    viewP.html(line, true);
   }
 
+}
 
-
+function analyzeData(){
+  var keys = Object.keys(allData);
+  var initialWeight = allData[keys[0]].weight;
+  var currentWeight = allData[keys[keys.length-1]].weight;
+  analysisParagraph.html("Total Weight Loss: "+(initialWeight-currentWeight)+"lbs");
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log('server is starting');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var server = app.listen(3000, listening);
 
@@ -9,37 +10,26 @@ function listening(){
 }
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+var jsonParser = bodyParser.json();
 
 var fs = require("fs");
 var data = fs.readFileSync("final.json");
 
 var weights = JSON.parse(data);
 
-
+//sending data to the client.
 app.get("/all", sendAll);
+
 function sendAll(request, response){
   response.send(weights);
   console.log("sent data");
 }
 
-//hi
 
-app.get("/add/:date/:weight", addWeight);
-function addWeight (request, response){
-
-  var data = request.params;
-  var date = data.date;
-  //enter error handling for dates.
-  var weight = data.weight;
-  //ender error handling for weights
-  weights[date].weight = Number(weight);
-  var reply = {
-    msg: "New weight added"
-  }
-  response.send(reply);
-}
-
+//searching the json data
 app.get("/search/:date", searchDate);
 function searchDate(request, response){
   var date = request.params.date;
@@ -58,22 +48,45 @@ function searchDate(request, response){
 
 }
 
-app.get("/add/:date/:attended", addAttended);
-function addAttended (request, response){
+app.post('/addP',jsonParser, addDatas);
+
+function addDatas (request, response){
+  console.log(request.body);
+  var reply = {status: "Data Added"}
+  response.send(reply);
+
+}
+
+
+
+
+
+
+
+//This is outdated code that adds weight/attendance in get requests - not good!
+
+app.get("/add/:date/:weight/:attended", addWeight);
+function addWeight (request, response){
 
   var data = request.params;
   var date = data.date;
   //enter error handling for dates.
+  var weight = data.weight;
   var attended = data.attended;
-  //ender error handling for attendance
-  if(attended == "yes"){
+  //ender error handling for weights
+  weights[date].weight = Number(weight);
+
+  if(attended == "true"){
       weights[date].bjj = true;
   }else{
       weights[date].bjj = false;
   }
 
   var reply = {
-    msg: "Attendance noted"
+    msg: "New data added",
+    date: date,
+    weight: weight,
+    bjj: attended
   }
   response.send(reply);
 }
